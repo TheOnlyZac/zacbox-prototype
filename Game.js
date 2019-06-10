9841283f229fb1955cad12b41ae62442ff39083d
Zac Krasnow <TheOnlyZac@gmail.com>
2019-06-10 00:09:20 -0400

    implemented Game class

--- Game.js
+++ Game.js
@@ -11,35 +11,70 @@ class WordSpudGame {
 
     /* Add a Player to the _players obj */
     addPlayer(id, name = "Player" + this._numPlayers) {
+        /* Add the new player to the lobby, and inc numPlayers */
         this._players[id] = new Player(id, name);
         this._numPlayers++;
+        console.log("user %s initialized as %s", id, this.getName(id));
+
+        
+        /* Check if this is the first player, and assign VIP */
+        if (this.numPlayers == 1) {
+            console.log(">new player is first, setting VIP");
+            this._players[id].isVip = true;
+        }
     }
 
     /* Delete a player from the _players obj */
     removePlayer(id) {
         if (this._players[id] == undefined) {
-            console.log("Failed to delete player w/ id %s", id);
-        } else {
-            delete this._players[id];
-            this._numPlayers--;
+            console.log("failed to delete player w/ id %s", id);
+            return;
+        }
+
+        /* Check if player leaving was VIP */
+        var needNewVip = false;
+        if (this._players[id].isVip)
+            needNewVip = true;
+
+        /* Delete player from the loby and dec numPlayers */
+        delete this._players[id];
+        this._numPlayers--;
+        console.log('user %s successfully disconnected', id);
+
+        /* Check if last player, or if we need to set new VIP */
+        if (this._numPlayers == 0) {
+            console.log(">leaving player was the last player");
+        } else if (needNewVip) {
+            var newVip = this.playerIds[0];
+            this._players[newVip].isVip = true;
+            console.log(">leaving player was VIP, the new one is %s", this.getName(newVip));
         }
     }
 
+    getName(id) {
+        return this._players[id].name;
+    }
+
     /* Return an array of playerIds */
-    get playerIds() { return Object.keys(_players); }
+    get playerIds() { return Object.keys(this._players); }
 
     /* Check if all players are ready */
     get isReady() {
         /* Iterate over every player */
         for (var i = 0; i < this.playerIds.length; i++) {
             /* Check if current player is NOT ready */
-            if (!(_players[this.playerIds[i]].isReady)) {
+            if (!(this._players[this.playerIds[i]].isReady)) {
                 /* Someone is not ready, return false */
                 return false;
             }
             /* Everyone is ready! */
             return true;
         }
+    }
+
+    nameChange(id, newName) {
+        console.log("changing name of %s from %s to %s", id, this.getName(id), newName);
+        this._players[id].name = newName;
     }
 }
 
--- server.js
+++ server.js
@@ -9,44 +9,24 @@ const Game = require('./Game.js');
 
 app.use(express.static(__dirname + "/public"));
 
-/* Create object to store all joining players */
-var players = {};
+/* Create new WordSpugGame instance */
+game = new Game();
 
 io.on('connection', function(socket) {
-    var pid = socket.id; // Get id of new connected client
-    players[pid] = new Player; // Create a new Player in the players obj
-    console.log("user %s initialized", pid);
-    if (Object.keys(players).length == 1) {
-        console.log("new player is first, setting VIP");
-        players[pid].isVip = true;
-    }
+    /* Get ID of new connected client */
+    var pid = socket.id;
+    /* Add the new Player to the lobby */
+    game.addPlayer(pid)
 
     /* On client disconnected */
     socket.on('disconnect', function() {
-        console.log('user %s disconnected', pid);
-
-        /* check if player leaving was VIP */
-        var needNewVip = false;
-        if (players[pid].isVip) {
-            needNewVip = true;
-        }
-
-        /* delete player from players obj */
-        delete players[pid];
-
-        /* check if last player, or if need new VIP */
-        if (Object.keys(players).length == 0) {
-            console.log("!! leaving player was the last player");
-        } else if (needNewVip) {
-            console.log("! leaving player was VIP, setting new one")
-            players[Object.keys(players)[0]].isVip = true;
-        }
+        game.removePlayer(pid);
     });
 
+
+    /* Change the name of a player */
     socket.on('set name', function(data) {
-        console.log("setting new name for %s: %s", pid, data.newName);
-        /* Set the new name for the Player in the players obj */
-        players[pid].name = data.newName;
+        game.nameChange(pid, data.newName);
 
         /* Tell that player they're all set, wait for game start */
         socket.emit('readyStart');
